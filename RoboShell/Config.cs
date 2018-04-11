6f082e0bbe465bca1f53b6a441c0b90487055a2c
oserikov <srkvoa@gmail.com>
2018-04-11 22:10:28 +0300

    tested interaction both ways

--- RoboShell/Config.cs
+++ RoboShell/Config.cs
@@ -13,7 +13,7 @@ namespace RoboShell
         public static int MinBoringSeconds = 10;
         public static int MaxBoringSeconds = 11;
         public static bool Headless = false;
-        public static string KBFileName = "imCupSc.brc";
+        public static string KBFileName = "gpioAsyncTesting.brc";
         public static int[] InputPinsNumbers = { 6, 13, 19, 26 };
     }
 }
