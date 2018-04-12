01528b6a4258d30094bf1f544d55af035e421123
mivallion <mivallion@gmail.com>
2018-04-12 21:18:11 +0300

    New static class for logs

---
+++ LogLib/Log.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using Serilog;
+using Serilog.Core;
+
+namespace LogLib
+{
+    public static class Log
+    {
+        public enum LogFlag
+        {
+            Debug,
+            Information,
+            Error
+        }
+        
+        //public static readonly Logger logger;
+
+        private static readonly string LogPath = Path.Combine(Environment.ExpandEnvironmentVariables("%userprofile"), "Documents");
+
+        private static Logger logger
+        {
+            //get => logger;
+            get => logger;
+            set => logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(LogPath + "\\logs\\Roboshell.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+        }
+
+        public static void Trace(string s)
+        {
+            logger.Information(s);
+            System.Diagnostics.Debug.WriteLine(s);
+        }
+
+        public static void Trace(string s, LogFlag flag)
+        {
+            switch (flag)
+            {
+                case LogFlag.Debug:
+                    logger.Debug(s);
+                    break;
+                case LogFlag.Information:
+                    logger.Information(s);
+                    break;
+                case LogFlag.Error:
+                    logger.Error(s);
+                    break;
+                default:
+                    logger.Error("Bad flag in Trace function");
+                    break;
+            }
+            System.Diagnostics.Debug.WriteLine(s);
+        }
+    }
+}
